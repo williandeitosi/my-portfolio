963adb7d2dfbdccc4af8a285d8c9e1585de03ff5
Willian <williandeitosi@gmail.com>
2023-11-02 11:41:15 -0300

    ajustando imagens nos cards

--- src/components/projects/Cards/Cards.jsx
+++ src/components/projects/Cards/Cards.jsx
@@ -1,11 +1,12 @@
 import style from "./card.module.css"
-import github from "../../../../public/github.png";
 
 
 const Cards = ({images, title, description, site, code}) => {
   return (
     <div className={style.card}>
-      <img className={style.photo} src={images} alt={title} />
+      <div className={style.cardPhoto}>
+        <img className={style.photo} src={images} alt={title} />
+      </div>
       <div className={style.info}>
         <h2>{title}</h2>
         <p>{description}</p>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,8 @@
 import Cards from "./Cards/Cards";
 import style from "./project.module.css";
 import todolist from "../../../public/imgProjects/todolist.png";
+import memoria from "../../../public/imgProjects/memoria.png";
+import cronometro from "../../../public/imgProjects/cronometro.png";
 
 const Projects = () => {
   return (
@@ -15,14 +17,21 @@ const Projects = () => {
           description="desenvolvimento de uma Lista de Tarefas utilizando a Stack MERN!"
         />
         <Cards
-          title="ToDo List"
-          images={todolist}
+          title="Jogo da memória"
+          images={memoria}
+          site="https://williandeitosi.github.io/jogo-da-memoria-reactjs/"
+          code="https://github.com/williandeitosi/jogo-da-memoria-reactjs"
+          description="jogo da memoria desenvolvido com React utilizando a componentização dos cards"
+        />
+        <Cards
+          title="Cronometro"
+          images={cronometro}
           site="https://mern-frontend-ivory.vercel.app/"
           code="https://github.com/williandeitosi/mern-frontend"
           description="desenvolvimento de uma Lista de Tarefas utilizando a Stack MERN!"
         />
         <Cards
-          title="ToDo List"
+          title="Player Music"
           images={todolist}
           site="https://mern-frontend-ivory.vercel.app/"
           code="https://github.com/williandeitosi/mern-frontend"
